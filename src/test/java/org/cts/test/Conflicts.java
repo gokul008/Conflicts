edc9fd53b7c7eb1e80146b8a50ce71dedf152e0e
gokul008 <sgokuls86@gmail.com>
2019-12-13 11:03:43 +0530

    conflict

--- src/test/java/org/cts/test/Conflicts.java
+++ src/test/java/org/cts/test/Conflicts.java
@@ -4,9 +4,9 @@ public class Conflicts {
 public static void main(String[] args) {
 System.out.println("conflicts ocuurs");
 System.out.println("it is");
+System.out.println("Gokul");
 	
-	
-	
+
 	
 }
 }
